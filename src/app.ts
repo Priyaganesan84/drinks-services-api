9abb0b554a9a736b0b8542bdbe7af95253805367
Priyaganesan84 <136799329+Priyaganesan84@users.noreply.github.com>
2023-08-15 18:57:47 +0100

    feat: Implement GET request for home endpoint and test

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import express from 'express';
 export const app = express();
 export const coffeeapp = express();
+app.use(express.json());
+coffeeapp.use(express.json());
+
 app.get('/', (req, res) => res.send('Welcome to the Drinks API!'));
+
 coffeeapp.get('/', (req, res) => res.send('I like coffee!'));
+
+
+app.get('/coffee', (req, res) => res.json({
+    drinkType: 'Coffee',
+    name: 'Latte',
+    }));
